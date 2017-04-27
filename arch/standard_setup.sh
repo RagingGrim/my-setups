8a59c2801b6ea458a486a9c2d2e361b0ea274106
RagingGrim <armandconnor.duplooy@gmail.com>
2017-04-27 11:48:21 +0200

    Fixed two typos and ln syntax.

--- arch/standard_setup.sh
+++ arch/standard_setup.sh
@@ -14,6 +14,6 @@ ln -f configs/zshrc ~/.zshrc
 
 # link termite
 mkdir -p ~/.config/termite
-ln -sf configs/termite_config ~/.config/termite/config
+ln -f configs/termite_config ~/.config/termite/config
 # twilight base 16: curl https://raw.githubusercontent.com/khamer/base16-termite/master/themes/base16-twilight.config >> ~/.config/termite/config
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
