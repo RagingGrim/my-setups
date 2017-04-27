519dfd7e44c9054c8d40433dde182cb81b3cbc71
RagingGrim <armandconnor.duplooy@gmail.com>
2017-04-27 12:01:10 +0200

    Reworked scripts.

---
+++ arch/link.sh
@@ -0,0 +1,10 @@
+
+# link zsh
+ln -f configs/zshrc ~/.zshrc
+
+
+
+# link termite
+mkdir -p ~/.config/termite
+ln -f configs/termite_config ~/.config/termite/config
+# twilight base 16: curl https://raw.githubusercontent.com/khamer/base16-termite/master/themes/base16-twilight.config >> ~/.config/termite/config
--- arch/standard_setup.sh
+++ arch/standard_setup.sh
@@ -3,17 +3,7 @@
 pacman -Syyu
 pacman -S clang atom chromium octave zsh nc nmap termite
 yaourt -S spotify oh-my-zsh-git
+git clone https://github.com/bhilburn/powerlevel9k.git /usr/share/oh-my-zsh/custom/themes/powerlevel9k
 
 
-echo "Creating system links to configs!"
-
-# link zsh
-ln -f configs/zshrc ~/.zshrc
-
-
-
-# link termite
-mkdir -p ~/.config/termite
-ln -f configs/termite_config ~/.config/termite/config
-# twilight base 16: curl https://raw.githubusercontent.com/khamer/base16-termite/master/themes/base16-twilight.config >> ~/.config/termite/config
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+echo "Run ./link.sh"
