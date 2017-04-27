c4ea220cdfde153021cc3d53322ba0db5bd3f5df
RagingGrim <armandconnor.duplooy@gmail.com>
2017-04-27 19:58:40 +0200

    Fixed setups.

--- arch/standard_setup.sh
+++ arch/standard_setup.sh
@@ -3,6 +3,7 @@
 pacman -Syyu
 pacman -S clang atom chromium octave zsh nc nmap termite
 yaourt -S spotify oh-my-zsh-git
+yaourt -S otf-hermit
 git clone https://github.com/bhilburn/powerlevel9k.git /usr/share/oh-my-zsh/custom/themes/powerlevel9k
 
 
