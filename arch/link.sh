d3a1bc5c48ae77d75b706090f0b2bfa8705a99ff
RagingGrim <armandconnor.duplooy@gmail.com>
2017-05-12 10:06:54 +0200

    Added i3.

--- arch/link.sh
+++ arch/link.sh
@@ -6,7 +6,12 @@ ln -f configs/zshrc ~/.zshrc
 # link termite
 mkdir -p ~/.config/termite
 ln -f configs/termite_config ~/.config/termite/config
-# twilight base 16: curl https://raw.githubusercontent.com/khamer/base16-termite/master/themes/base16-twilight.config >> ~/.config/termite/config
 
+# link neovim
 mkdir -p ~/.config/nvim
 ln -f configs/init.vim ~/.config/nvim/init.vim
+
+# link i3gaps && i3blocks
+mkdir -p ~/.config/i3
+ln -f configs/i3_config ~/.config/i3/config
+ln -f configs/i3blocks_config ~/.i3blocks.conf
