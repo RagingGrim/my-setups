c83b8d734540b15656741fc1ce384c24aa67295a
RagingGrim <armandconnor.duplooy@gmail.com>
2017-05-25 16:09:30 +0200

    Added dieter_custom.

--- arch/link.sh
+++ arch/link.sh
@@ -15,3 +15,6 @@ ln -f configs/init.vim ~/.config/nvim/init.vim
 mkdir -p ~/.config/i3
 ln -f configs/i3_config ~/.config/i3/config
 ln -f configs/i3blocks_config ~/.i3blocks.conf
+
+# zsh dieter customised.
+sudo ln -f configs/dieter_custom.zsh-theme /usr/share/dieter
